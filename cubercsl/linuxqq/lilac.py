ad0b3dd315a6a926bf87f23f0ab0e9e25cbcc18f
cubercsl <2014cais01@gmail.com>
2022-12-31 19:00:45 +0800

    fix typo

--- cubercsl/linuxqq/lilac.py
+++ cubercsl/linuxqq/lilac.py
@@ -17,13 +17,12 @@ def _get_md5_prefix():
 def pre_build():
     md5_prefix = _get_md5_prefix()
     print(f'Found md5 prefix: {md5_prefix}')
-    pkgver = _G.newver.lstrip('v').replace('-', '_')
-    update_pkgver_and_pkgrel(pkgver)
-    url = 'https://im.qq.com/rainbow/linuxQQDownload'
     for line in edit_file('PKGBUILD'):
         if line.startswith('_md5_prefix='):
             line = f'_md5_prefix={md5_prefix}'
         print(line)
+    pkgver = _G.newver.lstrip('v').replace('-', '_')
+    update_pkgver_and_pkgrel(pkgver)
 
 def post_build():
     git_pkgbuild_commit()
