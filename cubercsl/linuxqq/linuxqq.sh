9ff56d29c2e87d0fdf64493cda45e71a6453f4f5
cubercsl <2014cais01@gmail.com>
2025-07-13 23:14:02 +0800

    update linuxqq

--- cubercsl/linuxqq/linuxqq.sh
+++ cubercsl/linuxqq/linuxqq.sh
@@ -2,6 +2,7 @@
 
 if [ -d ~/.config/QQ/versions ]; then
 	find ~/.config/QQ/versions -name sharp-lib -type d -exec rm -r {} \; 2>/dev/null
+	find ~/.config/QQ/versions -name libssh2.so.1 -type f -exec rm {} \; 2>/dev/null
 fi
 
 rm -rf ~/.config/QQ/crash_files/*
@@ -10,8 +11,7 @@ XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-~/.config}
 
 if [[ -f "${XDG_CONFIG_HOME}/qq-flags.conf" ]]; then
 	mapfile -t QQ_USER_FLAGS <<<"$(grep -v '^#' "${XDG_CONFIG_HOME}/qq-flags.conf")"
-	echo "User flags:" "${QQ_USER_FLAGS[@]}"
+	echo "User flags:" ${QQ_USER_FLAGS[@]}
 fi
 
-exec /opt/QQ/qq "${QQ_USER_FLAGS[@]}" "$@"
-
+exec /opt/QQ/qq ${QQ_USER_FLAGS[@]} "$@"
