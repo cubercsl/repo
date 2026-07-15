5f4388ec6f401f19dd4e0eb9bab9fce254e98a42
cubercsl <cubercsl@gmail.com>
2026-07-15 22:06:01 +0800

    Delete cubercsl/linuxqq directory

--- cubercsl/linuxqq/lilac.py
+++
@@ -1,9 +0,0 @@
-from lilaclib import *
-
-def pre_build():
-    pkgver = _G.newver.lstrip('v').replace('-', '_')
-    update_pkgver_and_pkgrel(pkgver)
-
-def post_build():
-    git_pkgbuild_commit()
-
